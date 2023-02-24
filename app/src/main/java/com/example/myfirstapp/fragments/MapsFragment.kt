23dd57f0f32731bea3420c58b6c4e51f8374e810
ismailkhanova <ismailkhanova_i@iuca.kg>
2023-02-24 20:30:19 +0600

    myLocationMaps

--- app/src/main/java/com/example/myfirstapp/fragments/MapsFragment.kt
+++ app/src/main/java/com/example/myfirstapp/fragments/MapsFragment.kt
@@ -1,15 +1,94 @@
 package com.example.myfirstapp.fragments
 
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.fragment.app.Fragment
+//import com.example.myfirstapp.R
+//
+//class MapsFragment : Fragment() {
+//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_maps, container, false)
+//    }
+//}
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.myfirstapp.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+class MapsFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    @SuppressLint("MissingPermission")
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            // Permission was granted, enable location on the map
+            googleMap.isMyLocationEnabled = true
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+
+        // Check for location permission
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Enable location on the map
+            googleMap.isMyLocationEnabled = true
+
+            // Get the user's current location
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    // Create a LatLng object from the location
+                    val userLocation = LatLng(location.latitude, location.longitude)
+
+                    // Add a marker at the user's location
+                    googleMap.addMarker(MarkerOptions().position(userLocation).title("Your Location"))
+
+                    // Move the camera to the user's location
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 12f))
+                }
+            }
+        } else {
+            // Request location permission
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
 }
+
