c3c076b2e04f3eaf656b2c04b3fcbb3156442c1b
ismailkhanova <ismailkhanova_i@iuca.kg>
2023-03-13 21:07:52 +0600

    ExpensesBefore change

--- app/src/main/java/com/example/myfirstapp/adapters/ExpenseDataStore.kt
+++ app/src/main/java/com/example/myfirstapp/adapters/ExpenseDataStore.kt
@@ -7,49 +7,6 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 
-//interface ExpenseDataChangeListener {
-//    fun onExpenseDataChanged()
-//}
-//
-//class ExpenseDataStore(private val context: Context) {
-//
-//    private val expenses = mutableListOf<Expense>()
-//    private var listener: ExpenseDataChangeListener? = null
-//
-//    fun addExpense(expense: Expense) {
-//        expenses.add(expense)
-//        saveExpenses()
-//        listener?.onExpenseDataChanged()
-//    }
-//
-//    fun getExpenses(): List<Expense> {
-//        return expenses
-//    }
-//
-//    fun setListener(listener: ExpenseDataChangeListener?) {
-//        this.listener = listener
-//    }
-//
-//    private fun saveExpenses() {
-//        val sharedPreferences = context.getSharedPreferences("expense_data_store", Context.MODE_PRIVATE)
-//        val editor = sharedPreferences.edit()
-//        val gson = Gson()
-//        val json = gson.toJson(expenses)
-//        editor.putString("expenses", json)
-//        editor.apply()
-//    }
-//
-//    init {
-//        val sharedPreferences = context.getSharedPreferences("expense_data_store", Context.MODE_PRIVATE)
-//        val gson = Gson()
-//        val json = sharedPreferences.getString("expenses", null)
-//        if (json != null) {
-//            val type = object : TypeToken<List<Expense>>() {}.type
-//            expenses.addAll(gson.fromJson(json, type))
-//        }
-//    }
-//}
-
 interface ExpenseDataChangeListener {
     fun onExpenseDataChanged()
 }
